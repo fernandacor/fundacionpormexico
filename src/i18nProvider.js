f55aa76a5ede3aad295c11b1af719fa372ff5840
Sebastian Moncada <sebas.200221@gmail.com>
2023-09-05 19:23:35 -0600

    Ya funciona con el paquete

--- src/i18nProvider.js
+++ src/i18nProvider.js
@@ -1,16 +1,14 @@
+import es from "@blackbox-vision/ra-language-spanish";
 import polyglotI18nProvider from "ra-i18n-polyglot";
 import en from "ra-language-english";
-import es from "ra-language-spanish";
-import spanishMessages from "./spanishMessages";
 
-const translations = { es, en, spanishMessages };
+const translations = { es, en };
 
 export const i18nProvider = polyglotI18nProvider(
   (locale) => translations[locale],
-  "spanishMessages", // default locale
+  "es", // default locale
   [
     { locale: "en", name: "English" },
     { locale: "es", name: "Español" },
-    { locale: "spanishMessages", name: "Español nuestro" },
   ]
 );
