3a4c2db4061d2d3248696ead51cecfe59bff0f19
Samuel Acevedo <a01026893@tec.mx>
2023-09-06 20:14:33 -0600

    Cambiada el Login, a ver si les gusta

--- src/MyLoginPage.tsx
+++ src/MyLoginPage.tsx
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
-import { useLogin} from 'react-admin';
+import * as React from 'react';
+import { useState } from 'react';
+import { Button, TextField, Grid, Paper, Typography } from '@mui/material';
+import { useLogin } from 'react-admin';
 
-const LoginPage: React.FC = () => {
+const MaterialUILoginPage = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const login = useLogin();
@@ -9,38 +11,53 @@ const LoginPage: React.FC = () => {
   const handleLogin = async () => {
     try {
       await login({ email, password });
-      // El inicio de sesión fue exitoso, puedes redirigir al usuario a la página principal o realizar otras acciones necesarias.
     } catch (error) {
-      // Manejar errores aquí (por ejemplo, mostrar un mensaje de error).
+      console.error('Error al iniciar sesión:', error);
     }
   };
 
   return (
-    <div>
-      <h1>Login</h1>
-      <form>
-        <div>
-          <label>Username:</label>
-          <input
-            type="text"
+    <Grid
+      container
+      justifyContent="center"
+      alignItems="center"
+      style={{ height: '100vh' }}
+    >
+      <Grid item xs={10} sm={6} md={4}>
+        <Paper elevation={3} style={{ padding: '20px' }}>
+          <Typography variant="h5" gutterBottom>
+            Iniciar Sesión
+          </Typography>
+          <TextField
+            fullWidth
+            label="Correo Electrónico"
+            variant="outlined"
+            margin="normal"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
-        </div>
-        <div>
-          <label>Password:</label>
-          <input
+          <TextField
+            fullWidth
+            label="Contraseña"
             type="password"
+            variant="outlined"
+            margin="normal"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
-        </div>
-        <button type="button" onClick={handleLogin}>
-          Login
-        </button>
-      </form>
-    </div>
+          <Button
+            variant="contained"
+            color="primary"
+            fullWidth
+            style={{ marginTop: '20px' }}
+            onClick={handleLogin}
+          >
+            Iniciar Sesión
+          </Button>
+        </Paper>
+      </Grid>
+    </Grid>
   );
 };
 
-export default LoginPage;
+export default MaterialUILoginPage;
