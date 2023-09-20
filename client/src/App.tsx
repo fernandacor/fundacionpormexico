444b0ddd7c78dfc9a450a1815e7c35895deb2215
Samuel Acevedo <a01026893@tec.mx>
2023-09-19 20:10:40 -0600

    Arreglado

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,9 +8,10 @@ import {
 import { dataProvider } from "./dataProvider";
 import MyLoginPage from "./MyLoginPage";
 import { authProvider } from './authProvider';
+import { UsersList } from "./users";
 
 export const App = () => (
   <Admin authProvider={authProvider} dataProvider={dataProvider} loginPage={MyLoginPage}>
-    <Resource name="test" list={ListGuesser} />
+    <Resource name="users" list={UsersList} />
   </Admin>
 );
