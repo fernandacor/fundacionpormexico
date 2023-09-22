aedb71aa71398abf5598fc215668b0df3515a67e
Fernanda Cantú <fercantu56@gmail.com>
2023-09-21 21:55:43 -0600

    archivos de prueba ya no son necesarios

--- src/tests/multiply.test.js
+++
@@ -1,6 +0,0 @@
-const multiply = require('./multiply');
-// import { multiply } from './multiply';
-
-test('multiply 1 * 2 to equal 2', () => {
-    expect(multiply(1, 2)).toBe(2);
-});
--- src/tests/sum.ts
+++
@@ -1,5 +0,0 @@
-function sumarNumeros(numero1: number, numero2: number): number {
-  return numero1 + numero2;
-}
-
-module.exports = sumarNumeros;
