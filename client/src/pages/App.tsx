825658f144fb587678246b94a9fb8d106157fd88
Samuel Acevedo <a01026893@tec.mx>
2023-09-21 19:02:40 -0600

    Merge completo con dev

--- client/src/pages/App.tsx
+++ client/src/pages/App.tsx
@@ -1,18 +1,25 @@
 import {
   Admin,
+  EditGuesser,
+  ListGuesser,
   Resource,
+  ShowGuesser,
+  CustomRoutes,
 } from "react-admin";
-import MyLoginPage from "./MyLoginPage";
-import { authProvider } from "../providers/authProvider";
+import {  Route} from 'react-router-dom';
 import { dataProvider } from "../providers/dataProvider";
-import { UsersList } from "../lists/usersList";
+import MyLoginPage from "./MyLoginPage";
+import authProvider from '../providers/authProvider';
+import { UsersList } from "../users";
+import { TicketsCreate, TicketsEdit, TicketsList } from "../tickets";
+import Registrarse from "../registrarse";
 
 export const App = () => (
-  <Admin
-    authProvider={authProvider}
-    dataProvider={dataProvider}
-    loginPage={MyLoginPage}
-  >
+  <Admin authProvider={authProvider} dataProvider={dataProvider}>
     <Resource name="users" list={UsersList} />
+    <Resource name="tickets" list={TicketsList} edit={TicketsEdit} create={TicketsCreate} />
+    <CustomRoutes>
+      <Route path="/registrarse"  element={<Registrarse />}/>
+    </CustomRoutes>
   </Admin>
 );
