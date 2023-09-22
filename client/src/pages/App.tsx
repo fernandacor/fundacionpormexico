1343572c55ad07168e566d08e4cdd55fbf1d941e
Sebastian Moncada <sebas.200221@gmail.com>
2023-09-21 19:25:41 -0600

    Organizar

--- client/src/pages/App.tsx
+++ client/src/pages/App.tsx
@@ -6,20 +6,25 @@ import {
   ShowGuesser,
   CustomRoutes,
 } from "react-admin";
-import {  Route} from 'react-router-dom';
+import { Route } from "react-router-dom";
 import { dataProvider } from "../providers/dataProvider";
 import MyLoginPage from "./MyLoginPage";
-import authProvider from '../providers/authProvider';
-import { UsersList } from "../users";
-import { TicketsCreate, TicketsEdit, TicketsList } from "../tickets";
-import Registrarse from "../registrarse";
+import authProvider from "../providers/authProvider";
+import { UsersList } from "../lists/users";
+import { TicketsCreate, TicketsEdit, TicketsList } from "../lists/tickets";
+import Registrarse from "./registrarse";
 
 export const App = () => (
   <Admin authProvider={authProvider} dataProvider={dataProvider}>
     <Resource name="users" list={UsersList} />
-    <Resource name="tickets" list={TicketsList} edit={TicketsEdit} create={TicketsCreate} />
+    <Resource
+      name="tickets"
+      list={TicketsList}
+      edit={TicketsEdit}
+      create={TicketsCreate}
+    />
     <CustomRoutes>
-      <Route path="/registrarse"  element={<Registrarse />}/>
+      <Route path="/registrarse" element={<Registrarse />} />
     </CustomRoutes>
   </Admin>
 );
