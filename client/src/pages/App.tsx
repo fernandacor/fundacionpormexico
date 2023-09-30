5919ae666d5b41814674696ddba54e16a193b604
Sebastian Moncada <sebas.200221@gmail.com>
2023-09-30 15:29:24 -0600

    Quitar cosas que ya no se usaban del import

--- client/src/pages/App.tsx
+++ client/src/pages/App.tsx
@@ -1,10 +1,9 @@
-import { Admin, CustomRoutes, Resource } from "react-admin";
-import { Route } from "react-router-dom";
+import { Admin, Resource } from "react-admin";
 import { TicketsCreate, TicketsEdit, TicketsList } from "../lists/tickets";
-import {UsersList, UsersEdit, UsersCreate}from "../lists/usersList";
+import { UsersCreate, UsersEdit, UsersList } from "../lists/usersList";
 import authProvider from "../providers/authProvider";
 import { dataProvider } from "../providers/dataProvider";
-import MyLoginPage from "./MyLoginPage"
+import MyLoginPage from "./MyLoginPage";
 
 export const App = () => (
   <Admin authProvider={authProvider} dataProvider={dataProvider} loginPage={MyLoginPage}>
