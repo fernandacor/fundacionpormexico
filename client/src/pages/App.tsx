72972d94e01798226016b8178bc662b32ad38e1e
Sebastian Moncada <sebas.200221@gmail.com>
2023-10-01 20:03:34 -0600

    Ordenar

--- client/src/pages/App.tsx
+++ client/src/pages/App.tsx
@@ -2,12 +2,21 @@ import { Admin, Resource } from "react-admin";
 import { TicketsCreate, TicketsEdit, TicketsList } from "../lists/tickets";
 import { UsersCreate, UsersEdit, UsersList } from "../lists/usersList";
 import authProvider from "../providers/authProvider";
-import { dataProvider } from "../providers/dataProvider";
-import MyLoginPage from "./MyLoginPage";
+import dataProvider from "../providers/dataProvider";
+import LoginPage from "./Login";
 
 export const App = () => (
-  <Admin authProvider={authProvider} dataProvider={dataProvider} loginPage={MyLoginPage}>
-    <Resource name="users" list={UsersList} edit={UsersEdit} create={UsersCreate}/>
+  <Admin
+    authProvider={authProvider}
+    dataProvider={dataProvider}
+    loginPage={LoginPage}
+  >
+    <Resource
+      name="users"
+      list={UsersList}
+      edit={UsersEdit}
+      create={UsersCreate}
+    />
     <Resource
       name="tickets"
       list={TicketsList}
