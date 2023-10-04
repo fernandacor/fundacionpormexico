d4d1045154f676e00a1dc99a3f63974781ea27e1
Sebastian Moncada <sebas.200221@gmail.com>
2023-10-03 18:27:41 -0600

    Depurar archivos necesarios para el botón de theme

--- client/src/components/MyAppBar.tsx
+++ client/src/components/MyAppBar.tsx
@@ -1,9 +1,3 @@
 import { AppBar, ToggleThemeButton } from "react-admin";
-import { defaultDarkTheme } from "react-admin";
 
-const darkTheme = defaultDarkTheme;
-
-export const MyAppBar = () => (
-    <AppBar>
-    </AppBar>
-    );
+export const MyAppBar = () => <AppBar toolbar={<ToggleThemeButton />} />;
