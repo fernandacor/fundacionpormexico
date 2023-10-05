75bd3d6cebdcce1f1d8ffaa8cd5cbd6f4f5f6cfb
Sebastian Moncada <sebas.200221@gmail.com>
2023-10-04 21:05:21 -0600

    Hace el cambio de theme en MUI y TW

--- client/src/hooks/useTheme.tsx
+++ client/src/hooks/useTheme.tsx
@@ -1,11 +1,22 @@
 import { useTheme } from "react-admin";
 
 const ThemeToggler = () => {
-  const [theme, setTheme] = useTheme();
+  const [, setTheme] = useTheme("dark");
+  const bodyTheme = document.getElementById("body").className;
+
+  const changeTheme = () => {
+    if (bodyTheme == "light") {
+      document.getElementById("body").className = "dark";
+      setTheme("dark");
+    } else {
+      document.getElementById("body").className = "light";
+      setTheme("light");
+    }
+  };
 
   return (
-    <button onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
-      {theme === "dark" ? "Switch to light theme" : "Switch to dark theme"}
+    <button onClick={changeTheme}>
+      {bodyTheme === "light" ? "Switch to dark theme" : "Switch to light theme"}
     </button>
   );
 };
