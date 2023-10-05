a1cfe4a412ef5d5ea8aba923e0525add82c37c3f
Sebastian Moncada <sebas.200221@gmail.com>
2023-10-04 21:05:06 -0600

    Pequeño cambio en como muestra el ticket

--- client/src/components/Ticket.tsx
+++ client/src/components/Ticket.tsx
@@ -8,7 +8,7 @@ const Ticket = () => {
     <div className="grid grid-cols-3 gap-6">
       {data.map((ticket) => (
         <Link to={`/tickets/${ticket.id}`} key={ticket.id}>
-          <div className="rounded-2xl shadow p-3 h-full hover:scale-105 transition flex flex-col gap-2 bg-gray-100 dark:bg-gray-900 group">
+          <div className="rounded-2xl shadow p-3 hover:scale-105 transition flex flex-col gap-2 bg-gray-100 dark:bg-gray-900 group">
             <div className="flex flex-row gap-2">
               <div className="bg-white shadow-lg w-14 h-14 rounded-2xl flex flex-none flex-col items-center justify-center">
                 <p className="font-black text-blue-700 text-xl leading-tight">
