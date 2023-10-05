6fa669c82844ec088654b44c543c6a7ccebfa9f7
Samuel Acevedo <a01026893@tec.mx>
2023-10-04 20:48:42 -0600

    YA FUNCIONAAAA AHHHHH

--- client/src/pages/App.tsx
+++ client/src/pages/App.tsx
@@ -1,4 +1,5 @@
 import { Admin, Layout, LayoutProps, Resource, usePermissions } from "react-admin";
+import { useEffect, useState} from "react";
 import { MyAppBar } from "../components/MyAppBar";
 import authProvider from "../providers/authProvider";
 import dataProvider from "../providers/dataProvider";
@@ -7,48 +8,34 @@ import { TicketsCreate, TicketsEdit, TicketsList } from "../resources/Tickets";
 import { UsersCreate, UsersEdit, UsersList } from "../resources/Users";
 import LoginPage from "./Login";
 
+const userRole = localStorage.getItem("permissions");
+
 const myLayout = (props: LayoutProps) => (
   <Layout {...props} appBar={MyAppBar} />
 );
 
-const { isLoading, permissions } = usePermissions();
-
-export const App = () => {
-  const { isLoading, permissions } = usePermissions();
-
-  if (isLoading) {
-    return <div>Waiting for permissions...</div>;
-  }
-
-  return (
-    <Admin
-      authProvider={authProvider}
-      dataProvider={dataProvider}
-      i18nProvider={i18nProvider}
-      loginPage={LoginPage}
-      layout={myLayout}
-      darkTheme={{ palette: { mode: "dark" } }}
-    >
-      {permissions => (
-        <>
-          {/* Restrict access to the edit view to admin only */}
-          <Resource
-            name="tickets"
-            list={TicketsList}
-            edit={TicketsEdit}
-            create={TicketsCreate}
-          />
-          {/* Only include the categories resource for admin users */}
-          {permissions === 'Ejecutivo' && (
-            <Resource
-              name="users"
-              list={UsersList}
-              edit={UsersEdit}
-              create={UsersCreate}
-            />
-          )}
-        </>
-      )}
-    </Admin>
-  );
-};
+export const App = () => (
+  <Admin
+    authProvider={authProvider}
+    dataProvider={dataProvider}
+    i18nProvider={i18nProvider}
+    loginPage={LoginPage}
+    layout={myLayout}
+    darkTheme={{ palette: { mode: "dark" } }}
+  >
+    {userRole === "Ejecutivo" && (
+      <Resource
+        name="users"
+        list={UsersList}
+        edit={UsersEdit}
+        create={UsersCreate}
+      />
+    )}
+    <Resource
+      name="tickets"
+      list={TicketsList}
+      edit={TicketsEdit}
+      create={TicketsCreate}
+    />
+  </Admin>
+);
