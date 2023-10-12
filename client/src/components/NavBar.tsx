2355f5d6a35303b5adfbb08178283952389df8ec
Sebastian Moncada <sebas.200221@gmail.com>
2023-10-11 19:19:10 -0600

    Un poco más de separación en los botones de navbar

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -4,7 +4,7 @@ import Logout from "./Logout";
 
 const NavBar = () => {
   return (
-    <nav className="flex flex-row px-2 gap-2 h-fit items-center justify-end sticky top-0 right-0 z-40 backdrop-blur-xl bg-neutral-100/50 dark:bg-neutral-950/50 rounded-b-lg py-1">
+    <nav className="flex flex-row px-2 gap-5 h-fit items-center justify-end sticky top-0 right-0 z-40 backdrop-blur-xl bg-neutral-100/50 dark:bg-neutral-950/50 py-1">
       {/* <TitlePortal /> */}
       {/* <LocalesMenuButton /> */}
       <ThemeToggler />
